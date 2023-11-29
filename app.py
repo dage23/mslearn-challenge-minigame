65021d1882c32ad230f7b3e373cc9e120e033139
dage23 <46784507+dage23@users.noreply.github.com>
2023-11-29 14:47:16 +0000

    Added main menu to app.py

--- app.py
+++ app.py
@@ -2,10 +2,36 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See LICENSE in the project root for license information.
 #-----------------------------------------------------------------------------------------
+def continue_playing():
+    command = input('Do you want to continue playing? Yes/No \n')
+    response = True
+    if command == "Yes" or command == "yes":
+        response = True
+    elif command == "No" or command == "no":
+        response = False
+    else: 
+        print("Value not valid \n")
+        continue_playing()
+    return (response)
 
-from flask import Flask
-app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return app.send_static_file("index.html")
+def main():
+    print('Hi! Welcome to Rock, Paper, Scissors')
+    input('Press Enter to play')
+
+    play = True
+    list_decision = ["rock","paper","scissors"]
+    score= 0
+    while play:
+        user_decision=input('rock, paper, scissors, SHOOT!\n')
+
+
+        score+=1
+        play = continue_playing()
+        if play == False:
+            print("Your score was " + str(score) + "\n")
+            input('Thanks for playing!')
+            break
+
+if __name__ == "__main__":
+    main()
